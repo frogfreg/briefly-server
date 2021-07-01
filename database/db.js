2c8a7f3d23f5cca651fca9335725d0ab291dabe8
Guillermo Fernandez <bengallme@gmail.com>
2021-07-01 17:11:27 -0500

    Added some comments to db.js

--- database/db.js
+++ database/db.js
@@ -1,14 +1,15 @@
-const { Pool }  = require("pg");
+const { Pool } = require("pg");
 
+//These are just development values, clearly
 const pool = new Pool({
-  user: 'postgres',
-  host: 'localhost',
-  database: 'briefly',
-  password: 'nyantaro',
-})
+  user: "postgres",
+  host: "localhost",
+  database: "briefly",
+  password: "nyantaro",
+});
 
 module.exports = {
   query: (text, params) => {
     return pool.query(text, params);
-  }
-}
+  },
+};
